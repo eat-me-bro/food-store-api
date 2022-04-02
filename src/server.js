eb5bc1c6d4583266f796cd24add45544f029651f
Koraltan Kaynak <koraltan.kaynak@gmail.com>
2022-04-02 06:53:57 +0200

    origin cross update

--- src/server.js
+++ src/server.js
@@ -7,10 +7,19 @@ const app = express()
 
 // Access Origin
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
+
+    const allowedOrigins = ['http://localhost:4200', 'http://192.168.178.40:4200'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
+    // res.header("Access-Control-Allow-Origin", "http://localhost:4200", "http://192.168.178.40:4200")
     res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS")
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
     next()
 })
+app.disable('x-powered-by')
 
 // Public Static File Folder
 let public_folder = path.join(__dirname, '..', '/public/')
@@ -40,37 +49,40 @@ app.get("/", async (req, res) => {
 app.get("/foodstores", async (req, res) => {
     res.setHeader('Content-Type', 'application/json')
     
-    let dummyFoodStores = {
-        foodStores: [ 
-            { id: 1,
+    let dummyFoodStores = [ 
+            { 
+                id: 1,
                 foodStore: "Casino",
                 location: {
-                    lon: 123.123,
-                    lat: 456.456
+                    lon: "123.123",
+                    lat: "456.456"
                 },
                 favorite: true,
                 lastVisite: "2022-30-03"     
             },
-            { id: 2,
+            { 
+                id: 2,
                 foodStore: "Dori, Dori",
                 location: {
-                    lon: 123.123,
-                    lat: 456.456
+                    lon: "123.123",
+                    lat: "456.456"
                 },
                 favorite: true,
                 lastVisite: "2022-29-03"     
             },
-            { id: 3,
+            { 
+                id: 3,
                 foodStore: "Chipo",
                 location: {
-                    lon: 123.123,
-                    lat: 456.456
+                    lon: "123.123",
+                    lat: "456.456"
                 },
                 favorite: false,
                 lastVisite: "2022-28-03"     
-        },
+            },
         ]
-    }
+#
+    console.log("RESULT BACK TO..:", req.headers.host);
 
     res.status(200).send(JSON.stringify(dummyFoodStores))
 })
